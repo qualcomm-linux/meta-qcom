013da0d51ee608f05ffbbb8e83c9c491247cf46c
Kavinaya S <kavinaya@qti.qualcomm.com>
2026-01-05 10:37:21 +0530

    dtb_only_fitimage.py: support configs for base DTB and DTBOs
    FIT image generation only accounts for standalone DTBs. This is not
    sufficient for platforms that rely on modular hardware components,
    such as camera, display add-ons, which require overlays (DTBOs)
    to be applied on top of a base DTB.
    
    Enabling DTB + DTBO combinations allows us to leverage the base DTB
    while layering changes through overlays.
    
    Signed-off-by: Kavinaya S <kavinaya@qti.qualcomm.com>

--- lib/qcom/dtb_only_fitimage.py
+++ lib/qcom/dtb_only_fitimage.py
@@ -14,8 +14,8 @@ import os
 import shlex
 import subprocess
 import bb
-from typing import Tuple
-from oe.fitimage import ItsNodeRootKernel
+from typing import Tuple, List, Dict
+from oe.fitimage import ItsNodeRootKernel, ItsNodeConfiguration
 
 # Custom extension of ItsNodeRootKernel to inject compatible strings
 class QcomItsNodeRoot(ItsNodeRootKernel):
@@ -34,14 +34,27 @@ class QcomItsNodeRoot(ItsNodeRootKernel):
                          None, None, False, None)
 
         self._mkimage_extra_opts = []
-        self._dtbs = []
+        self._dtbs: List[Tuple[object, str]] = []
+        self._base_compats: Dict[str, str] = {}
+        self._overlay_groups: Dict[str, List[List[str]]] = {}
+        self._overlay_compats: Dict[str, str] = {}
 
     def set_extra_opts(self, mkimage_extra_opts):
         self._mkimage_extra_opts = shlex.split(mkimage_extra_opts) if mkimage_extra_opts else []
 
+    def set_base_compats(self, base_compats):
+        self._base_compats = base_compats or {}
+
+    def set_overlay_groups(self, overlay_groups):
+        self._overlay_groups = overlay_groups or {}
+
+    def set_overlay_compats(self, overlay_compats):
+        self._overlay_compats = overlay_compats or {}
+
     # Emit the DTB section for the FIT image
     def fitimage_emit_section_dtb(self, dtb_id, dtb_path,
-                                  compatible_str=None):
+                                  compatible_str=None,
+                                  dtb_type=None):
         load = None
         dtb_ext = os.path.splitext(dtb_path)[1]
 
@@ -52,11 +65,6 @@ class QcomItsNodeRoot(ItsNodeRootKernel):
         if load:
             opt_props["load"] = f"<{load}>"
 
-        if dtb_id == "qcom-metadata.dtb":
-            dtb_type = "qcom_metadata"
-        else:
-            dtb_type = "flat_dt"
-
         dtb_node = self.its_add_node_dtb(
             "fdt-" + dtb_id,
             "Flattened Device Tree blob",
@@ -65,16 +73,64 @@ class QcomItsNodeRoot(ItsNodeRootKernel):
             opt_props,
             compatible_str
         )
-        self._dtbs.append((dtb_node, compatible_str))
+        self._dtbs.append((dtb_node, dtb_id))
+
+    def _fitimage_emit_one_section_config(self, conf_node_name, dtb=None):
+        """Emit the fitImage ITS configuration section"""
+        opt_props = {}
+        conf_desc = []
+
+        if dtb:
+            conf_desc.append("FDT blob")
+            opt_props["fdt"] = dtb.name
+            if dtb.compatible:
+                opt_props["compatible"] = dtb.compatible
+
+        ItsNodeConfiguration(
+            conf_node_name,
+            self.configurations,
+            description="FDT Blob",
+            opt_props=opt_props
+        )
+
 
     def fitimage_emit_section_config(self):
-        counter = 0
-        for counter, (dtb_node, compatible_str) in enumerate(self._dtbs):
-            # conf-0 corresponds to qcom-metadata.dtb, skipping it as it's metadata-only
-            if counter == 0:
+        conf_idx = 1
+        for (dtb_node, dtb_id) in self._dtbs:
+            # Skip qcom metadata blob
+            if dtb_id == "qcom-metadata.dtb":
                 continue
-            conf_name = f"{self._conf_prefix}{counter}"
-            self._fitimage_emit_one_section_config(conf_name, dtb_node)
+
+            # Add conf- entries only for dtb
+            if dtb_id.endswith(".dtb"):
+                conf_name = f"{self._conf_prefix}{conf_idx}"
+                self._fitimage_emit_one_section_config(conf_name, dtb_node)
+
+                base_compat = self._base_compats.get(dtb_id, "")
+                conf_node = self.configurations.sub_nodes[-1]
+                if base_compat:
+                    conf_node.add_property('compatible', base_compat)
+
+                # overlay_groups["base.dtb"] = [["ovl.dtbo"], ["ovl1.dtbo","ovl2.dtbo"], ...]
+                for items in self._overlay_groups.get(dtb_id, []):
+                    fdt_base = f"fdt-{dtb_id}"
+                    fdt_ovls = [f"fdt-{ovl}" for ovl in items]
+                    fdtentries = [fdt_base] + fdt_ovls
+
+                    conf_idx += 1
+                    conf_name = f"{self._conf_prefix}{conf_idx}"
+                    self._fitimage_emit_one_section_config(conf_name, dtb_node)
+
+                    # Composite key: "abc.dtb+ovl.dtbo+..."
+                    lookup_key = "+".join([dtb_id] + items)
+                    compat_str = self._overlay_compats.get(lookup_key, "")
+
+                    conf_node = self.configurations.sub_nodes[-1]
+                    conf_node.add_property('fdt', fdtentries)
+                    if compat_str:
+                        conf_node.add_property('compatible', compat_str)
+
+                conf_idx += 1
 
     # Override mkimage assemble to inject extra opts
     def run_mkimage_assemble(self, itsfile, fitfile):
