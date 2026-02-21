qualcomm-linux/meta-qcom

commit 1cff0376792664392d62e1a615276d44d708082b

firmware-qcom-sdm845-hdk: package SLPI firmware for SDM845-HDK
SDM845-HDK is a development kit for the SDM845 / SDA845 platforms. As
most of the firmware for those devices is a part of linux-firmware,
package only possibly-device-specific firmware for the SLPI, Sensors
DSP.

Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>

recipes-bsp/firmware/firmware-qcom-sdm845-hdk.bb
@@ -1,0 +1,24 @@
+# Specify location of the corresponding NON-HLOS.bin file by adding
+# NHLOS_URI:pn-firmware-qcom-sdm845-hdk = "..."  to local.conf. Use "file://"
+# if the file is provided locally.
+
+DESCRIPTION = "QCOM Firmware for SDM845 HDK (aka HDK845) board"
+
+LICENSE = "CLOSED"
+
+FW_QCOM_NAME = "sdm845-hdk"
+FW_QCOM_DIR = "sdm845/Qualcomm/SDM845-HDK"
+
+# ADSP, CDSP, modem and WLAN are a part of linux-firmware
+FW_QCOM_LIST = "\
+    slpi.mbn slpir.jsn \
+"
+
+S = "${UNPACKDIR}"
+
+require recipes-bsp/firmware/firmware-qcom.inc
+require recipes-bsp/firmware/firmware-qcom-nhlos.inc
+
+SPLIT_FIRMWARE_PACKAGES = "\
+    linux-firmware-qcom-${FW_QCOM_NAME}-sensors \
+"

commit 90156902b206bd24f7092cfc253127219edc52fe

firmware-qcom-sdm845-hdk: fix path for the SLPI firmware
The recipe specifies FW_QCOM_DIR as a location of the device-specific
firmware, however firmware-qcom.inc uses FW_QCOM_SUBDIR. Correct the
variable name to move HDK-specific firmware (SLPI) to the correct
location.

Fixes: 1cff03767926 ("firmware-qcom-sdm845-hdk: package SLPI firmware for SDM845-HDK")
Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>

recipes-bsp/firmware/firmware-qcom-sdm845-hdk.bb
@@ -7,7 +7,7 @@ DESCRIPTION = "QCOM Firmware for SDM845 HDK (aka HDK845) board"
 LICENSE = "CLOSED"
 
 FW_QCOM_NAME = "sdm845-hdk"
-FW_QCOM_DIR = "sdm845/Qualcomm/SDM845-HDK"
+FW_QCOM_SUBDIR = "sdm845/Qualcomm/SDM845-HDK"
 
 # ADSP, CDSP, modem and WLAN are a part of linux-firmware
 FW_QCOM_LIST = "\